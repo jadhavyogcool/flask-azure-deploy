4f949b6e75c59dbfb718d4c8e573aa52e9fd3432
jadhavyogcool <jadhavyogcool@gmail.com>
2026-03-28 10:33:06 +0530

    CICD2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Flask app running on Azure 🚀"
+    return "This is a simple Flask app deployed to Azure 🚀"
 
 if __name__ == "__main__":
     app.run()
